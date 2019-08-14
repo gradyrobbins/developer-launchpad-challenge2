cb8afefb9f1eccc62a4d9523ccf642c08d9763fb
Grady Robbins <gradyrobbins13@gmail.com>
2019-08-14 17:06:42 -0500

    level 2 complete?

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 console.log("hello world main.js");
 
-let output = document.getElementById('display-section');
+let level1 = document.getElementById('level1');
+let level2 = document.getElementById('level2');
 
 let fruit = ["apple", "banana", "orange", "pear", "grape"];
 console.log('fruit: ', fruit)
 console.log("fruit.reverse() :", fruit.reverse())
 
-output.innerHTML += `LEVEL 1: <br />`
-output.innerHTML += `fruit:  [ ${fruit} ]`
-output.innerHTML += `<br/>`
-output.innerHTML += `fruit.reverse() :`
-output.innerHTML += fruit.reverse();
+level1.innerHTML += `LEVEL 1: <br />`
+level1.innerHTML += `fruit:  [ ${fruit} ]`
+level1.innerHTML += `<br/>`
+level1.innerHTML += `fruit.reverse() :`
+level1.innerHTML += fruit.reverse();
 
+
+function reverseString(str){
+    let revstr = "";
+    for(let i = str.length-1; i>=0; i--){
+        revstr = revstr+ str[i];
+    }
+    return revstr;
+}
+
+for (let i = 0; i < fruit.length; i++) {
+    console.log(reverseString(fruit[i]))
+    level2.innerHTML += reverseString(fruit[i] + ", ")
+}
